3f9a04d218fbdd624b8deaa3d3c0c984c62e0240
Ross Basarevych <basarevych@gmail.com>
2017-03-10 15:56:42 +0200

    No extra unicode in tree

--- commands/tree.js
+++ commands/tree.js
@@ -146,7 +146,7 @@ class Tree {
                 ' on ' + (tree.listenAddress ?
                     tree.listenAddress + ':' :
                     ((tree.listenPort && tree.listenPort[0] == '/') ? '' : '*:')) +
-                tree.listenPort || '*' + ' ⇒ ' +
+                tree.listenPort || '*' + ' -> ' +
                 (tree.type == this.Tree.Type.SERVER ? '[' : '') +
                 tree.serversNumber +
                 (tree.type == this.Tree.Type.SERVER ? ']' : '') +
