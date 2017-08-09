3faaf0a3849e7bf1e639e1ddeee92a744391d05d
Ross Basarevych <basarevych@gmail.com>
2017-08-09 14:33:43 +0300

    Forgotten absolute path

--- commands/stop.js
+++ commands/stop.js
@@ -68,7 +68,7 @@ class Stop {
             .then(result => {
                 if (result.code === 0) {
                     return this._runner.exec(
-                        'kill',
+                        path.join(__dirname, '..', 'bin', 'kill'),
                         [ '/var/run/bhid/daemon.pid', 'SIGTERM' ],
                         { pipe: process }
                     );
